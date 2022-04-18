b10e8bff2871eed8be5f9c9f7f2fdfa53ceb75ae
leventekeves <keveslev@gmail.com>
2022-04-18 18:11:32 +0200

    server config file restructure

--- server/config.js
+++ server/config.js
@@ -1,8 +1,9 @@
 const neo4j = require("neo4j-driver");
 
-const driver = neo4j.driver(
-  "neo4j+s://373bc408.databases.neo4j.io",
-  neo4j.auth.basic("neo4j", "jWPQTxy-r-EdIqJAHglPE9fCmIRs7Rcdy-TJeEknpgg")
-);
+const uri = "neo4j+s://373bc408.databases.neo4j.io";
+const username = "neo4j";
+const password = "jWPQTxy-r-EdIqJAHglPE9fCmIRs7Rcdy-TJeEknpgg";
+
+const driver = neo4j.driver(uri, neo4j.auth.basic(username, password));
 
 module.exports.driver = driver;
